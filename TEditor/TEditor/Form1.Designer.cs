fc0f3c8a0e64dccec9acc96dab42505f3a78aafd
KUBIXQAZ <kubajozwik20027@gmail.com>
2023-04-19 21:16:54 +0200

    version 1.0.0

---
+++ TEditor/TEditor/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox = new System.Windows.Forms.RichTextBox();
+            this.WindowBarPanel = new System.Windows.Forms.Panel();
+            this.fullscreenB = new System.Windows.Forms.Button();
+            this.minimizeB = new System.Windows.Forms.Button();
+            this.exitB = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WindowBarPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textbox
+            // 
+            this.textbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textbox.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.textbox.Location = new System.Drawing.Point(5, 35);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(790, 410);
+            this.textbox.TabIndex = 0;
+            this.textbox.Text = "";
+            this.textbox.TextChanged += new System.EventHandler(this.textbox_TextChanged);
+            // 
+            // WindowBarPanel
+            // 
+            this.WindowBarPanel.Controls.Add(this.label1);
+            this.WindowBarPanel.Controls.Add(this.button4);
+            this.WindowBarPanel.Controls.Add(this.button3);
+            this.WindowBarPanel.Controls.Add(this.button2);
+            this.WindowBarPanel.Controls.Add(this.button1);
+            this.WindowBarPanel.Controls.Add(this.fullscreenB);
+            this.WindowBarPanel.Controls.Add(this.minimizeB);
+            this.WindowBarPanel.Controls.Add(this.exitB);
+            this.WindowBarPanel.Location = new System.Drawing.Point(0, 0);
+            this.WindowBarPanel.Name = "WindowBarPanel";
+            this.WindowBarPanel.Size = new System.Drawing.Size(800, 30);
+            this.WindowBarPanel.TabIndex = 1;
+            // 
+            // fullscreenB
+            // 
+            this.fullscreenB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.fullscreenB.FlatAppearance.BorderSize = 0;
+            this.fullscreenB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fullscreenB.Location = new System.Drawing.Point(755, 8);
+            this.fullscreenB.Name = "fullscreenB";
+            this.fullscreenB.Size = new System.Drawing.Size(15, 15);
+            this.fullscreenB.TabIndex = 2;
+            this.fullscreenB.TabStop = false;
+            this.fullscreenB.UseVisualStyleBackColor = false;
+            this.fullscreenB.Click += new System.EventHandler(this.fullscreenB_Click);
+            // 
+            // minimizeB
+            // 
+            this.minimizeB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.minimizeB.FlatAppearance.BorderSize = 0;
+            this.minimizeB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeB.Location = new System.Drawing.Point(735, 8);
+            this.minimizeB.Name = "minimizeB";
+            this.minimizeB.Size = new System.Drawing.Size(15, 15);
+            this.minimizeB.TabIndex = 1;
+            this.minimizeB.TabStop = false;
+            this.minimizeB.UseVisualStyleBackColor = false;
+            this.minimizeB.Click += new System.EventHandler(this.minimizeB_Click);
+            // 
+            // exitB
+            // 
+            this.exitB.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.exitB.FlatAppearance.BorderSize = 0;
+            this.exitB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitB.Location = new System.Drawing.Point(775, 8);
+            this.exitB.Name = "exitB";
+            this.exitB.Size = new System.Drawing.Size(15, 15);
+            this.exitB.TabIndex = 0;
+            this.exitB.TabStop = false;
+            this.exitB.UseVisualStyleBackColor = false;
+            this.exitB.Click += new System.EventHandler(this.exitB_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.button1.Location = new System.Drawing.Point(5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 22);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.button2.Location = new System.Drawing.Point(163, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.button3.Location = new System.Drawing.Point(242, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 22);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "save as";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.button4.Location = new System.Drawing.Point(84, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(73, 22);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "close";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "words: 0 letters: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WindowBarPanel);
+            this.Controls.Add(this.textbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.WindowBarPanel.ResumeLayout(false);
+            this.WindowBarPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textbox;
+        private System.Windows.Forms.Panel WindowBarPanel;
+        private System.Windows.Forms.Button exitB;
+        private System.Windows.Forms.Button minimizeB;
+        private System.Windows.Forms.Button fullscreenB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TEditor/TEditor/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TEditor
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern int PostMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            WindowBarPanel.MouseDown += new MouseEventHandler(move_window);
+            label1.MouseDown += new MouseEventHandler(move_window);
+        }
+        #region resizing_window
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Transparent, Top());
+            e.Graphics.FillRectangle(Brushes.Transparent, Left());
+            e.Graphics.FillRectangle(Brushes.Transparent, Right());
+            e.Graphics.FillRectangle(Brushes.Transparent, Bottom());
+        }
+
+        private const int HTLEFT = 10;
+        private const int HTRIGHT = 11;
+        private const int HTTOP = 12;
+        private const int HTTOPLEFT = 13;
+        private const int HTTOPRIGHT = 14;
+        private const int HTBOTTOM = 15;
+        private const int HTBOTTOMLEFT = 16;
+        private const int HTBOTTOMRIGHT = 17;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+            if (m.Msg == 0x84)
+            {
+                var mp = this.PointToClient(Cursor.Position);
+
+                if (TopLeft().Contains(mp))
+                    m.Result = (IntPtr)HTTOPLEFT;
+                else if (TopRight().Contains(mp))
+                    m.Result = (IntPtr)HTTOPRIGHT;
+                else if (BottomLeft().Contains(mp))
+                    m.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (BottomRight().Contains(mp))
+                    m.Result = (IntPtr)HTBOTTOMRIGHT;
+                else if (Top().Contains(mp))
+                    m.Result = (IntPtr)HTTOP;
+                else if (Left().Contains(mp))
+                    m.Result = (IntPtr)HTLEFT;
+                else if (Right().Contains(mp))
+                    m.Result = (IntPtr)HTRIGHT;
+                else if (Bottom().Contains(mp))
+                    m.Result = (IntPtr)HTBOTTOM;
+            }
+        }
+
+        private Random rng = new Random();
+        public Color randomColour()
+        {
+            return Color.FromArgb(255, rng.Next(255), rng.Next(255), rng.Next(255));
+        }
+
+        const int ImaginaryBorderSize = 2;
+
+        public new Rectangle Top()
+        {
+            return new Rectangle(0, 0, this.ClientSize.Width, ImaginaryBorderSize);
+        }
+
+        public new Rectangle Left()
+        {
+            return new Rectangle(0, 0, ImaginaryBorderSize, this.ClientSize.Height);
+        }
+
+        public new Rectangle Bottom()
+        {
+            return new Rectangle(0, this.ClientSize.Height - ImaginaryBorderSize, this.ClientSize.Width, ImaginaryBorderSize);
+        }
+
+        public new Rectangle Right()
+        {
+            return new Rectangle(this.ClientSize.Width - ImaginaryBorderSize, 0, ImaginaryBorderSize, this.ClientSize.Height);
+        }
+
+        public Rectangle TopLeft()
+        {
+            return new Rectangle(0, 0, ImaginaryBorderSize, ImaginaryBorderSize);
+        }
+
+        public Rectangle TopRight()
+        {
+            return new Rectangle(this.ClientSize.Width - ImaginaryBorderSize, 0, ImaginaryBorderSize, ImaginaryBorderSize);
+        }
+
+        public Rectangle BottomLeft()
+        {
+            return new Rectangle(0, this.ClientSize.Height - ImaginaryBorderSize, ImaginaryBorderSize, ImaginaryBorderSize);
+        }
+
+        public Rectangle BottomRight()
+        {
+            return new Rectangle(this.ClientSize.Width - ImaginaryBorderSize, this.ClientSize.Height - ImaginaryBorderSize, ImaginaryBorderSize, ImaginaryBorderSize);
+        }
+        #endregion
+        private void move_window(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                PostMessage(Handle, 0xA1, 0x2, 0);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowBarPanel.BackColor = Color.Gray;
+
+            exitB.BackColor = Color.Red;
+            minimizeB.BackColor = Color.Yellow;
+            fullscreenB.BackColor = Color.Blue;
+
+            BackColor = Color.FromArgb(24, 25, 26);
+            textbox.BackColor = Color.FromArgb(34, 37, 41);
+            WindowBarPanel.BackColor = Color.FromArgb(13, 13, 13);
+
+            Color b_color = Color.FromArgb(27, 42, 56);
+            button1.BackColor = b_color;
+            button2.BackColor = b_color;
+            button3.BackColor = b_color;
+            button4.BackColor = b_color;
+            ForeColor = Color.FromArgb(214, 214, 214);
+            textbox.ForeColor = Color.FromArgb(214, 214, 214);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int x = Size.Width;
+            int y = Size.Height;
+
+            textbox.Width = x-10;
+            textbox.Height = y-40;
+
+            WindowBarPanel.Width = x;
+            exitB.Location = new Point(x-25, exitB.Location.Y);
+            fullscreenB.Location = new Point(exitB.Location.X - 20, fullscreenB.Location.Y);
+            minimizeB.Location = new Point(fullscreenB.Location.X - 20, minimizeB.Location.Y);
+        }
+        private void exitB_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void minimizeB_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+        private void fullscreenB_Click(object sender, EventArgs e)
+        {
+            if(WindowState == FormWindowState.Maximized) WindowState = FormWindowState.Normal;
+            else WindowState = FormWindowState.Maximized;
+        }
+
+        string Openedfilepath;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var file = openFileDialog.OpenFile();
+                string fileName = openFileDialog.FileName;
+                if (Path.GetExtension(fileName) == ".txt")
+                {
+                    string file_content = File.ReadAllText(fileName);
+                    textbox.Text = file_content;
+                    Openedfilepath = Path.GetFullPath(fileName);
+                    file.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string textbox_content = textbox.Text;
+            if (File.Exists(Openedfilepath))
+            {
+                File.WriteAllText(Openedfilepath, textbox_content);
+            } else
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text Files|*.txt";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(Path.GetFullPath(saveFileDialog.FileName), textbox_content);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string textbox_content = textbox.Text;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(Path.GetFullPath(saveFileDialog.FileName), textbox_content);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Openedfilepath = "";
+            textbox.Text = "";
+        }
+
+        private void textbox_TextChanged(object sender, EventArgs e)
+        {
+            string text = textbox.Text;
+
+            int letters = text.Length - text.Split(' ').Length + 1 - text.Split('\n').Length + 1;
+
+            int words = 0;
+            text = text.Trim();
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            words = text.Split(' ').Length;
+            if (text.Length == 0) words = 0;
+
+            label1.Text = "words: " + words + " letters: " + letters;
+        }
+    }
+}
